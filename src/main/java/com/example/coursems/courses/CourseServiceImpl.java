7514daae29860989d9b9d059d4c114cc2394c50f
olowo-coder <olowofela.olanrewaju@decagon.dev>
2022-03-16 19:57:51 +0100

    Using MS SQL sever as database

--- src/main/java/com/example/coursems/courses/CourseServiceImpl.java
+++ src/main/java/com/example/coursems/courses/CourseServiceImpl.java
@@ -1,18 +1,28 @@
 package com.example.coursems.courses;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class CourseServiceImpl implements CourseService{
 
+//    private static final Logger logger = LoggerFactory.getLogger(CourseServiceImpl.class);
+
     private final CourseRepository courseRepository;
+    private final Tracer tracer;
 
     @Autowired
-    public CourseServiceImpl(CourseRepository courseRepository) {
+    public CourseServiceImpl(CourseRepository courseRepository, Tracer tracer) {
         this.courseRepository = courseRepository;
+        this.tracer = tracer;
     }
 
     @Override
@@ -23,6 +33,18 @@ public class CourseServiceImpl implements CourseService{
 
     @Override
     public Course getById(Long id) {
+        log.trace("trace: log before custom span");
+        log.info("info: log before custom span");
+        Span newSpan = this.tracer.nextSpan().name("custom-log");
+        try(Tracer.SpanInScope ws = this.tracer.withSpan(newSpan.start())){
+            newSpan.tag("custom-tag", "###333###");
+            log.info("info: log in custom span");
+        }
+        finally {
+            //remember to end the span
+            newSpan.end();
+        }
+        log.info("info: log after custom span");
         return courseRepository.findById(id).orElse(null);
     }
 
